47f1933fea6c70c0ecf84b2ca74dd756d0d537e1
Muhammad Ahsan <59800198+MuhammadAhsanDonuts@users.noreply.github.com>
2022-03-23 18:55:00 +0500

    signup-logic-has-been-pushed

--- ecommerce-backend/routes/user.js
+++ ecommerce-backend/routes/user.js
@@ -1,38 +1,9 @@
 const express = require('express');
 const router = express.Router(); //
 const User = require('../models/user');
-const bcrypt = require('bcrypt');
+const {signup} = require('../controllers/auth')
 
 
-
-router.post('/signup', (req, res) => {
-    User.findOne({ email: req.body.email})
-    .exec(async (error, user) => {
-        if(user) return res.status(400).json({ message: "User already exists"}); 
-    })    
-
-        const {
-            firstName, lastName, email, password, 
-        } = req.body; 
-    
-        const _user = new User({
-            firstName, 
-            lastName,
-            email,
-            password,
-            username: Math.random().toString()
-        })
-        _user.save((error, data) => {
-            if(error){ 
-                console.log(error);
-                
-                return res.status(400).json({message: "Something went wrong"})
-            }
-             
-            if (data) return res.status(200).json({user: data}); 
-        })
-
-
-})
+router.post('/signup', signup); 
 
 module.exports = router; 
